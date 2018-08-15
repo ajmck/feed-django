8830330b9c398227a720cd7e7c8f9e82293d6c92
Alex McKirdy <ajmck@users.noreply.github.com>
2018-08-15 21:46:07 +1200

    Added placeholder icons for voting and details (also, FUCK CSS)

--- web/feed/settings.py
+++ web/feed/settings.py
@@ -96,14 +96,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, "static"),
-#         "/opt/feed/static"
-#         ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-print("STATIC_ROOT: " + str(STATIC_ROOT))
 
+# Custom parameters
 POST_BODY_LENGTH = 300
