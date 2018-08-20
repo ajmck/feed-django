03efecc5ea967a69622d3e3eb8e96ac4296b8da0
Alex McKirdy <ajmck@users.noreply.github.com>
2018-08-20 23:19:14 +1200

    Location button changes colour

--- web/core/static/core/js/location.js
+++ web/core/static/core/js/location.js
@@ -1,8 +1,10 @@
 // copied from https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
-window.onload  = function geoFindMe() {
+function geoFindMe() {
 
 	if (!navigator.geolocation){
 		console.log("Geolocation is not supported by your browser");
+		document.getElementById("form-location-button").className = "input-group-prepend btn btn-warning"
+
 		return;
 	}
 
@@ -12,12 +14,15 @@ window.onload  = function geoFindMe() {
 
 		document.getElementById("form-lat").value = latitude
 		document.getElementById("form-lon").value = longitude
-
+		document.getElementById("form-location-button").className = "input-group-prepend btn btn-success"
 	}
 
 	function error() {
 		console.log("Unable to retrieve your location");
+		document.getElementById("form-location-button").className = "input-group-prepend btn btn-warning"
 	}
 
 	navigator.geolocation.getCurrentPosition(success, error);
 }
+
+window.onload = geoFindMe()
