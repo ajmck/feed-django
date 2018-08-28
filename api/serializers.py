80652c6fe6abfa97f0d590a7b684298852d4c29d
Alex McKirdy <ajmck@users.noreply.github.com>
2018-08-28 21:14:33 +1200

    Added geoserialiser

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,19 @@
 from core.models import Post, Comment
 from feed.settings.base import POST_BODY_LENGTH
 from rest_framework import serializers
+from rest_framework_gis import serializers as geoserializers
 
 
-class PostSerializer(serializers.Serializer):
+class PostAbstractPOSTSerializer(serializers.BaseSerializer):
     body = serializers.CharField(max_length=POST_BODY_LENGTH)
-    pub_date = serializers.DateTimeField(required=False)
+    latitude = serializers.FloatField(required=False)
+    longitude = serializers.FloatField(required=False)
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
 
     # def update(self, instance, validated_data):
     #     pass
