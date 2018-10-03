8460b406445527b197d7f04f40cfd01a3e1a69c9
ajmck <ajmck@users.noreply.github.com>
2018-10-03 23:15:00 +1300

    Meshblocks in admin, and correctly adding from QGIS (must be MultiPolygons)

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GeoModelAdmin
 from django.contrib.gis.db import models
 from django.contrib.gis.admin import OSMGeoAdmin
 
 # unsure if below line works right, may need to be admin widget
 from django.contrib.gis.forms.widgets import OSMWidget
+
+from core.models import Meshblock
 from .models import Post, Comment
 # Register your models here.
 
@@ -16,8 +19,6 @@ class CommentInline(admin.TabularInline):
     formfield_overrides = {models.PointField: {'widget': OSMWidget}}
 
 
-
-
 class PostAdmin(admin.ModelAdmin):
     model = Post
     readonly_fields = ["pub_date"]
@@ -27,5 +28,13 @@ class PostAdmin(admin.ModelAdmin):
     formfield_overrides = {models.PointField: {'widget': OSMWidget}}
 
 
+class MeshblockAdmin(OSMGeoAdmin):
+    model = Meshblock
+    # below line fails due to being multipolygon (or something)
+    # readonly_fields = ["geo"]
+    modifiable = False
+    # formfield_overrides = {models.MultiPolygonField: {'widget': OSMWidget}}
+
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Meshblock, MeshblockAdmin)
