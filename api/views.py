bca3b451370810462d7898ac265e8b4a7c1cff25
ajmck <ajmck@users.noreply.github.com>
2018-10-11 22:01:45 +1300

    Add a distance filter to meshblock view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView
-from rest_framework_gis.filters import InBBoxFilter
+from rest_framework_gis.filters import InBBoxFilter, DistanceToPointFilter
 from core.models import Post, Comment, Meshblock
 from rest_framework import viewsets
 from .serializers import PostSerializer, MeshblockSerializer, PostGeoJsonSerializer
@@ -13,12 +13,17 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class MeshblockViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Filter by extent - inbboxfilter - ?in_bbox=a,b,c,d
+    # Filter by distance - DistanceToPointFilter - ?dist=(metres)&point=(lon, lat)
+    """
     queryset = Meshblock.objects.all()
     serializer_class = MeshblockSerializer
     bbox_filter_field = 'geom'
-    filter_backends = (InBBoxFilter, )
+    distance_filter_field = 'geom'   # will this need to be a new point / centroid?
+    filter_backends = (InBBoxFilter, DistanceToPointFilter, )
     bbox_filter_include_overlapping = True
-
+    distance_filter_convert_meters = True  # ensures dist query search is performed in metres
 
 class PostGeoJsonViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
